3d36cdb0f2d3bc134c0349393c1876d2b794c94a
sunjianyi <sunjianyi@xsteach.com>
2016-06-20 18:34:45 +0800

    init project

---
+++ AliyunOSS.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * 邢帅教育
+ * 本源代码由邢帅教育及其作者共同所有，未经版权持有者的事先书面授权，
+ * 不得使用、复制、修改、合并、发布、分发和/或销售本源代码的副本。
+ * @copyright Copyright (c) 2013 xsteach.com all rights reserved.
+ */
+namespace xsteach\oss;
+
+/**
+ * Class AliyunOSS
+ * @package xsteach\oss
+ * @author Choate <choate.yao@gmail.com>
+ */
+class AliyunOSS
+{
+    function __construct() {
+        include_once __DIR__ . DIRECTORY_SEPARATOR . 'aliyun.php';
+    }
+}
